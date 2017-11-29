ec216c00d467793260a5db1bbea55c7de7ee7618
Ted Sauerbrey <tedsauerbrey@Teds-MacBook-Air.local>
2017-11-29 13:48:07 -0600

    Initial Commit

---
+++ TapYourTempo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapYourTempo
+//
+//  Created by Ted Sauerbrey on 11/29/17.
+//  Copyright © 2017 Ted Sauerbrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
